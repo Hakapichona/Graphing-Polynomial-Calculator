2746702acd210658b32e93ea0896be7e15c9b28c
Laptop-Matty <matiasjavier1291@gmail.com>
2023-10-24 16:12:02 -0300

    Función Recta1 (recta con dos puntos), sería bueno que prueben con varios puntos

--- Codigo/Funciones/Recta1.py
+++ Codigo/Funciones/Recta1.py
@@ -2,7 +2,10 @@ from Codigo.Clases.Punto import Punto
 
 def Recta1(puntoA: Punto, puntoB: Punto):
     if puntoA.x == puntoB.x:
-        return f"x = {puntoA.x}"
+        if puntoA.x == 0:
+            return f"x=0"
+        else:
+            return f"x-{puntoA.x}=0"
     else:
         m = (puntoB.y - puntoA.y) / (puntoB.x - puntoA.x)
         if m % 1 == 0:
@@ -16,30 +19,36 @@ def Recta1(puntoA: Punto, puntoB: Punto):
     if m > 0:
         if m == 1:
             if termino_independiente == 0:
-                return f"x - y = 0"
+                return f"x-y=0"
             else:
-                return f"x - y + {termino_independiente} = 0"
+                return f"x-y+{termino_independiente}=0"
         else:
             if termino_independiente == 0:
-                return f"{m}x - y = 0"
+                return f"{m}x-y=0"
             else:
-                return f"{m}x - y + {termino_independiente} = 0"
+                if termino_independiente > 0:
+                    return f"{m}x-y+{termino_independiente}=0"
+                else:
+                    return f"{m}x-y-{-1*termino_independiente}=0"
     elif m < 0:
         if m == -1:
             if termino_independiente == 0:
-                return f"- x + y = 0"
+                return f"-x+y=0"
             else:
-                return f"- x + y - {termino_independiente} = 0"
+                return f"-x+y-{termino_independiente}=0"
         else:
             if termino_independiente == 0:
-                return f"{-1*m}x + y = 0"
+                return f"{-1*m}x+y=0"
             else:
                 if termino_independiente > 0:
-                    return f"{-1*m}x + y - {termino_independiente} = 0"
+                    return f"{-1*m}x+y-{termino_independiente}=0"
                 else:
-                    return f"{-1 * m}x + y + {-1*termino_independiente} = 0"
+                    return f"{-1 * m}x+y+{-1*termino_independiente}=0"
     elif m == 0:
         if termino_independiente == 0:
-            return f"y = 0"
+            return f"y=0"
         else:
-            return f"y - {termino_independiente} = 0"
+            return f"y-{termino_independiente}=0"
+
+ecuacion = Recta1(Punto(0,0),Punto(0,1))
+print(ecuacion)
