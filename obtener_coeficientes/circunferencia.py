296a6b7f65ac47628afb68c72f9e41faea3148ab
wiskeylazo <148465989+wiskeylazo@users.noreply.github.com>
2023-11-08 10:48:37 -0300

    circunferencia.py

--- obtener_coeficientes/circunferencia.py
+++ obtener_coeficientes/circunferencia.py
@@ -1,14 +1,18 @@
+#CORREGIDO
+
 class Circunferencia:
 
-    def __init__(self, xa, yb, c):
+    def __init__(self, a, b, c):
 
-        self.a = xa
-        self.b = yb
-        self.c = c
+        self.d = a
+        self.e = b
+        self.f = c
 
     
-def sacar_coef(ecuacion):
-        
+def crear_circunferencia():
+
+    ecuacion= input(f"Ingrese la circunferencia en el formato 'x2 + y2 + ax + by + c = 0': ")
+    
     coef_final = []
 
     coeficientes = []
@@ -21,11 +25,15 @@ def sacar_coef(ecuacion):
 
     ec = ec.split('+')
 
+    i = 4
+
     for termino in ec:
 
         if '-' in termino:
 
             termino = termino.split('-')
+
+            i = i + 1
 
             coeficientes.append(termino[0])
 
@@ -36,27 +44,89 @@ def sacar_coef(ecuacion):
         else:
 
             coeficientes.append(termino)
+
+    if 'x2' in coeficientes and 'y2' in coeficientes:
+
+        #Aqui almacenamos en list todos los terminos de la ecuacion por separado
+        list = []
+        for termino in coeficientes:
+            if termino != '' and termino != 'x2' and termino != 'y2':
+                for coef in termino:
+                    list.append(coef)        
+
+        for termino in coeficientes:
+
+            if termino != '' and termino != 'x2' and termino != 'y2':
+
+                char = ""
+
+                for coef in termino:
+
+                    if coef != 'x' and coef != 'y':
+
+                        char += coef
+
+                    else:
+
+                        break
+
+                if not char or char == '-' or char == '+':
+
+                    char += '1'
+
+                coef_final.append(int(char))
+
+        if 'x' not in list and 'y' in list:
+
+            return Circunferencia(0, coef_final[0], coef_final[1])
+
+        elif 'y' not in list and 'x' in list:
+
+            return Circunferencia(coef_final[0], 0, coef_final[1])
+
+        elif 'x' not in list and 'y' not in list:
+
+            return Circunferencia(0, 0, coef_final[0])
+        
+        else:
+
+            return Circunferencia(coef_final[0], coef_final[1], coef_final[2])
     
-    for termino in coeficientes:
+    else:
 
-        if termino != '' and termino != 'x2' and termino != 'y2':
+        print("Error. Ingrese la ecuacion de una circunferencia.")
 
-            char = ""
+def print_circunferencia(c1):
 
-            for coef in termino:
+    if c1.d < 0 and c1.e < 0 and c1.f < 0:
 
-                if coef != 'x' and coef != 'y':
+        print( "circunferencia: x2 + y2 - %dx - %dy - %d = 0" %( abs(c1.d), abs(c1.e), abs(c1.f) ) )
 
-                    char += coef
+    elif c1.d < 0 and c1.e < 0:
 
-                else:
+        print( "circunferencia: x2 + y2 - %dx - %dy + %d = 0" %( abs(c1.d), abs(c1.e), c1.f ) )
 
-                    break
+    elif c1.d < 0 and c1.f < 0:
 
-            if not char or char == '-' or char == '+':
+        print( "circunferencia: x2 + y2 - %dx + %dy - %d = 0" %( abs(c1.d), c1.e, abs(c1.f) ) )
 
-                char += '1'
+    elif c1.e < 0 and c1.f < 0:
 
-            coef_final.append(int(char))
+        print( "circunferencia: x2 + y2 + %dx - %dy - %d = 0" %( c1.d, abs(c1.e), abs(c1.f) ) )
 
-    return Circunferencia(coef_final[0], coef_final[1], coef_final[2])
+    elif c1.d < 0:
+
+        print( "circunferencia: x2 + y2 - %dx + %dy + %d = 0" %( abs(c1.d), c1.e, c1.f ) )
+
+    elif c1.e < 0:
+
+        print( "circunferencia: x2 + y2 + %dx - %dy + %d = 0" %( c1.d, abs(c1.e), c1.f ) )
+
+    elif c1.f < 0:
+
+        print( "circunferencia: x2 + y2 + %dx + %dy - %d = 0" %(c1.d, c1.e, abs(c1.f) ) )
+
+
+c1 = crear_circunferencia()
+
+print_circunferencia(c1)
